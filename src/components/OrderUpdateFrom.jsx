07c1e5a4d047409cee315d7d294e2fecf10d6531
shekhmonjurul <shekhmonjurul@gmail.com>
2025-09-04 18:49:01 +0600

    noting

--- src/components/OrderUpdateFrom.jsx
+++ src/components/OrderUpdateFrom.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import {
   Table,
   TableBody,
@@ -15,19 +15,44 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  ListItem,
+  List,
+  ListItemText
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import SearchIcon from "@mui/icons-material/Search";
 
+const products = [
+  { id: 1, name: "Laptop", price: 50000 },
+  { id: 2, name: "Mobile", price: 20000 },
+  { id: 3, name: "Keyboard", price: 2000 },
+  { id: 4, name: "Mouse", price: 1000 },
+];
+
+
 function OrderUpdateFrom() {
+  const [searchProduct, setSearchProduct] = useState("")
+  const [show, setShow] = useState(true)
+
   const [status, setStatus] = useState("")
-  const handelChange = (e)=>{
+  const handelChange = (e) => {
     setStatus(e.target.value)
   }
+
+  const filterProduct = useMemo(() => {
+    return products.filter(product => product.name.toLowerCase().includes(searchProduct.toLowerCase()))
+  }, [searchProduct])
 
   const handleDelete = (id) => {
     console.log("Delete row:", id);
   };
+
+  const handelSerach = (e) => {
+    setSearchProduct(e.target.value)
+    setShow(false)
+    console.log("filter: ", filterProduct);
+
+  }
 
   const columns = [
     {
@@ -50,6 +75,9 @@ function OrderUpdateFrom() {
       width: 120,
       type: "number",
       editable: true,
+      renderCell: (params)=>{
+        <Box>+</Box>
+      }
     },
     { field: "sellPrice", headerName: "Sell Price", width: 130, type: "number" },
     {
@@ -105,6 +133,8 @@ function OrderUpdateFrom() {
         <TextField
           variant="outlined"
           placeholder="All Product Here"
+          value={searchProduct}
+          onChange={handelSerach}
           fullWidth
           InputProps={{
             sx: { height: 40 },
@@ -115,6 +145,11 @@ function OrderUpdateFrom() {
             ),
           }}
         />
+        <List>
+          {filterProduct.map((product) =><ListItem key={product.id}> 
+            {show? <ListItemText primary={"emty"} sx={{display: "none"}}/> : <ListItemText primary={product.name} />}
+          </ListItem>)}
+        </List>
       </Paper>
 
       {/* DataGrid */}
@@ -129,7 +164,7 @@ function OrderUpdateFrom() {
           sx={{
             "& .editable-cell": {
               backgroundColor: "#fff4e5",
-              border: "1px dashed #ff9800",
+              border: "1px dashed #79736aff",
             },
           }}
         />
@@ -147,18 +182,18 @@ function OrderUpdateFrom() {
                 <TableRow>
                   <TableCell>
                     <FormControl fullWidth>
-                        <InputLabel id="status">Selctec Status</InputLabel>
-                        <Select 
+                      <InputLabel id="status">Selctec Status</InputLabel>
+                      <Select
                         id="status"
                         value={status}
                         label={"Where Delevary"}
                         onChange={handelChange}
-                        >
-                          <MenuItem value={"In Side Dhaka"}>In Side Dhaka</MenuItem>
-                          <MenuItem value={"Dhaka"}>Dhaka</MenuItem>
-                          <MenuItem value={"Out Side Dhaka"}>Out Side Dhaka</MenuItem>
-                            
-                        </Select>
+                      >
+                        <MenuItem value={"In Side Dhaka"}>In Side Dhaka</MenuItem>
+                        <MenuItem value={"Dhaka"}>Dhaka</MenuItem>
+                        <MenuItem value={"Out Side Dhaka"}>Out Side Dhaka</MenuItem>
+
+                      </Select>
                     </FormControl>
                   </TableCell>
                 </TableRow>
@@ -216,412 +251,6 @@ function TableData({ value }) {
   );
 }
 
+
+
 export default OrderUpdateFrom;
-
-
-
-// import React, { useState } from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableContainer,
-//   TableRow,
-//   Paper,
-//   TextField,
-//   Button,
-//   Avatar,
-//   Box,
-//   Grid,
-//   InputAdornment,
-// } from "@mui/material";
-// import { DataGrid } from "@mui/x-data-grid";
-// import SearchIcon from "@mui/icons-material/Search";
-
-// function OrderUpdateFrom() {
-//   const handleDelete = (id) => {
-//     console.log("Delete row:", id);
-//   };
-
-//   const columns = [
-//     {
-//       field: "image",
-//       headerName: "Image",
-//       width: 100,
-//       renderCell: (params) => (
-//         <Avatar
-//           alt={params.row.productName}
-//           src={params.value}
-//           sx={{ width: 40, height: 40 }}
-//           variant="square"
-//         />
-//       ),
-//     },
-//     { field: "productName", headerName: "Product Name", width: 200 },
-//     {
-//       field: "quantity",
-//       headerName: "Quantity",
-//       width: 120,
-//       type: "number",
-//       editable: true,
-//     },
-//     { field: "sellPrice", headerName: "Sell Price", width: 130, type: "number" },
-//     {
-//       field: "discount",
-//       headerName: "Discount",
-//       width: 120,
-//       type: "number",
-//       editable: true,
-//     },
-//     { field: "subTotal", headerName: "Sub Total", width: 150, type: "number" },
-//     {
-//       field: "action",
-//       headerName: "Action",
-//       width: 150,
-//       renderCell: (params) => (
-//         <Button
-//           variant="contained"
-//           color="error"
-//           size="small"
-//           onClick={() => handleDelete(params.row.id)}
-//         >
-//           Delete
-//         </Button>
-//       ),
-//     },
-//   ];
-
-//   const rows = [
-//     {
-//       id: 1,
-//       image: "/product-img/trimmer.jpg",
-//       productName: "Laptop",
-//       quantity: 2,
-//       sellPrice: 55000,
-//       discount: 5000,
-//       subTotal: 100000,
-//     },
-//     {
-//       id: 2,
-//       image: "/product-img/trimmer.jpg",
-//       productName: "Mouse",
-//       quantity: 3,
-//       sellPrice: 800,
-//       discount: 0,
-//       subTotal: 2400,
-//     },
-//   ];
-
-//   return (
-//     <Box sx={{ mt: 2, mr: 2, ml: 2 }}>
-//       <Paper elevation={5} sx={{ p: 2, mb: 2 }}>
-//         {/* Search Box */}
-//         <TextField
-//           variant="outlined"
-//           placeholder="Search by Number or Invoice ID"
-//           fullWidth
-//           InputProps={{
-//             sx: { height: 40 },
-//             endAdornment: (
-//               <InputAdornment position="end">
-//                 <SearchIcon />
-//               </InputAdornment>
-//             ),
-//           }}
-//         />
-//       </Paper>
-
-//       {/* Data Grid */}
-//       <Paper elevation={5} sx={{ p: 2 }}>
-//         <DataGrid
-//           rows={rows}
-//           columns={columns}
-//           autoHeight
-//           getCellClassName={(params) =>
-//             params.colDef.editable ? "editable-cell" : ""
-//           }
-//           sx={{
-//             "& .editable-cell": {
-//               backgroundColor: "#fff4e5", // হালকা কমলা শেড
-//               border: "1px dashed #ff9800",
-//             },
-//           }}
-//         />
-//       </Paper>
-
-//       {/* নিচের ইনফো সেকশন */}
-//       <Grid
-//         container
-//         spacing={2}
-//         mt={5}
-//         sx={{
-//           placeItems: "center",
-//           justifyContent: "center",
-//         }}
-//       >
-//         <Grid item xs={8} component={Paper} elevation={5}>
-//           <Box>
-//             <Table>
-//               <TableBody>
-//                 <TableData value={"Monjurul Islam"} />
-//                 <TableData value={"01716550180"} />
-//                 <TableData value={"In side Dhaka"} />
-//                 <TableData value={"Processing"} />
-//               </TableBody>
-//             </Table>
-//           </Box>
-//         </Grid>
-
-//         <Grid item xs={4} component={Paper} elevation={5}>
-//           <Box>
-//             <Table sx={{ border: "1px solid black" }}>
-//               <TableBody>
-//                 <PaymentInfo leftValue={"Sub Total"} rightValue={590} />
-//                 <PaymentInfo leftValue={"Shipping Fee"} rightValue={80} />
-//                 <PaymentInfo leftValue={"Discount"} rightValue={0} />
-//                 <PaymentInfo leftValue={"Total"} rightValue={670} />
-//               </TableBody>
-//             </Table>
-//           </Box>
-//         </Grid>
-
-//         <Grid item xs={12} mb={2} component={Paper} elevation={5}>
-//           <Button
-//             variant="contained"
-//             sx={{
-//               width: "100%",
-//               "&:focus": {
-//                 outline: "none",
-//               },
-//             }}
-//           >
-//             Update Order
-//           </Button>
-//         </Grid>
-//       </Grid>
-//     </Box>
-//   );
-// }
-
-// function PaymentInfo({ leftValue, rightValue }) {
-//   return (
-//     <TableRow>
-//       <TableCell>{leftValue}</TableCell>
-//       <TableCell>{rightValue}</TableCell>
-//     </TableRow>
-//   );
-// }
-
-// function TableData({ value, children }) {
-//   return (
-//     <TableRow>
-//       <TableCell>
-//         {value}
-//         {children}
-//       </TableCell>
-//     </TableRow>
-//   );
-// }
-
-// export default OrderUpdateFrom;
-
-
-
-
-
-
-
-
-// import React, { useState } from "react"
-// import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Button, Avatar, InputAdornment, Box, Grid } from "@mui/material"
-// import SearchBox from "./mui-styles/SearchBox"
-// import { DataGrid } from "@mui/x-data-grid"
-
-
-// function OrderUpdateFrom() {
-
-// const columns = [
-//   {
-//     field: 'image',
-//     headerName: 'Image',
-//     width: 100,
-//     renderCell: (params) => (
-//       <Avatar
-//         alt={params.row.productName}
-//         src={params.value}
-//         sx={{ width: 40, height: 40 }}
-//         variant="square"
-//       />
-//     ),
-//   },
-//   { field: 'productName', headerName: 'Product Name', width: 200 },
-//   { field: 'quantity', headerName: 'Quantity', width: 120, type: 'number', editable: true },
-//   { field: 'sellPrice', headerName: 'Sell Price', width: 130, type: 'number' },
-//   { field: 'discount', headerName: 'Discount', width: 120, type: 'number', editable: true },
-//   { field: 'subTotal', headerName: 'Sub Total', width: 150, type: 'number' },
-//   {
-//     field: 'action',
-//     headerName: 'Action',
-//     width: 150,
-//     renderCell: (params) => (
-//       <Button
-//         variant="contained"
-//         color="error"
-//         size="small"
-//         onClick={() => handleDelete(params.row.id)}
-//       >
-//         Delete
-//       </Button>
-//     ),
-//   },
-// ];
-
-
-// const rows = [
-//   {
-//     id: 1,
-//     image: '/product-img/trimmer.jpg',
-//     productName: 'Laptop',
-//     quantity: 2,
-//     sellPrice: 55000,
-//     discount: 5000,
-//     subTotal: 100000,
-//   },
-//   {
-//     id: 2,
-//     image: '/product-img/trimmer.jpg',
-//     productName: 'Mouse',
-//     quantity: 3,
-//     sellPrice: 800,
-//     discount: 0,
-//     subTotal: 2400,
-//   },
-// ];
-
-
-
-
-
-//     return (
-       
-//         <Box sx={{ mt: 2, mr: 2, ml: 2 }} >
-            
-//                 <Box component={Paper} elevation={5}>
-//                     <Box sx={{ mt: 2, mb: 2 }}>
-//                     <SearchBox width={"100%"} />
-//                 </Box>
-//                 <Box component={Paper} elevation={5}>
-//                     <DataGrid
-//                         rows={rows}
-//                         columns={columns}
-//                     />
-//                 </Box>
-//                 </Box>
-          
-//             <Grid container spacing={2} mt={5} sx={{
-//                 placeItems: "center",
-//                 justifyContent: "center"
-//             }}>
-//                 <Grid size={8} component={Paper} elevation={5}>
-                    
-//                         <Box>
-//                             <Table>
-//                                 <TableBody>
-//                                     <TableData value={"Monjurul Islam"} />
-//                                     <TableData value={"01716550180"} />
-//                                     <TableData value={"In side Dhaka"} />
-//                                     <TableData value={"Processing"} />
-//                                 </TableBody>
-//                             </Table>
-//                         </Box>  
-//                 </Grid>
-//                 <Grid size={4} component={Paper} elevation={5}>
-//                     <Box>
-//                         <Table sx={{
-//                             border: "1px solid black"
-//                         }}>
-//                             <TableBody sx={{
-//                                 border: "1px solid black"
-//                             }}>
-//                                 <PaymentInfo leftValue={"Sub Total"} rightValue={590} />
-//                                 <PaymentInfo leftValue={"Shipping Fee"} rightValue={80} />
-//                                 <PaymentInfo leftValue={"Discount"} rightValue={0} />
-//                                 <PaymentInfo leftValue={"Total"} rightValue={670} />
-//                             </TableBody>
-//                         </Table>
-//                     </Box>
-
-//                 </Grid>
-//                 <Grid size={12} mb={2} component={Paper} elevation={5}>
-//                         <Button variant="contained" sx={{
-//                             width: "100%",
-//                             "&:focus": {
-//                                 outline: "none"
-//                             },
-//                         }}>Update Order</Button>
-//                 </Grid>
-//             </Grid>
-
-//         </Box>
-//     )
-
-// }
-
-// function PaymentInfo({ leftValue, rightValue }) {
-//     return (
-//         <TableRow>
-//             <TableCell>{leftValue}</TableCell>
-//             <TableCell>{rightValue}</TableCell>
-//         </TableRow>
-
-//     )
-// }
-// function TableCellCenterText(props) {
-//     return (
-//         <TableCell sx={{
-//             textAlign: "center",
-//             verticalAlign: "middle",
-//             ...props.sx
-//         }}>
-//             {props.children}
-
-//         </TableCell>
-//     )
-// }
-
-
-
-// function DisplayUserInfo({ label, defaultValue }) {
-//     return (
-//         <div className="table">
-//             <TextField
-//                 label={label}
-//                 defaultValue={defaultValue}
-//                 slotProps={{
-//                     input: {
-//                         readOnly: true
-//                     }
-//                 }}
-//                 sx={{
-//                     width: "100%",
-//                     height: "20px",
-//                     textAlign: "center"
-//                 }}
-//             />
-//         </div>
-
-
-//     )
-// }
-
-// function TableData({ value, children }) {
-//     return (
-//         <TableRow>
-//             <TableCell>
-//                 {value}
-//                 {children}
-//             </TableCell>
-//         </TableRow>
-//     )
-// }
-// export default OrderUpdateFrom
